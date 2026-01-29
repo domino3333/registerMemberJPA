5ca6201847bd2832ed6d0ab02398050827448bcf
Minho Kang <busster@naver.com>
2026-01-29 12:48:11 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/registerspringjpa/RegisterSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.registerspringjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterSpringJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterSpringJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/registerspringjpa/RegisterSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registerspringjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterSpringJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
