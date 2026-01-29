5e1cd86ef8df3e522097ecababc59043f892be68
Minho Kang <busster@naver.com>
2026-01-29 13:51:25 +0900

    프로젝트 초기 세팅 완료 / main 페이지 실행 확인

---
+++ src/main/java/com/example/registerspringjpa/domain/Member.java
@@ -0,0 +1,14 @@
+package com.example.registerspringjpa.domain;
+
+
+import lombok.Data;
+
+@Data
+public class Member {
+    private int no;
+    private String name;
+    private String password;
+    private int age;
+
+
+}
---
+++ src/main/java/com/example/registerspringjpa/service/MemberServiceImpl.java
@@ -0,0 +1,52 @@
+package com.example.registerspringjpa.service;
+
+
+import com.example.registerspringjpa.domain.Member;
+import com.example.registerspringjpa.repository.MemberDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MemberServiceImpl implements MemberService{
+
+    @Autowired
+    private MemberDAO memberDAO;
+
+
+    @Override
+    public int insertMember(Member member) throws Exception {
+
+        return memberDAO.insertMember(member);
+    }
+
+    @Override
+    public Member selectByNo(int no) throws Exception {
+
+        return memberDAO.selectByNo(no);
+    }
+
+    @Override
+    public int updateMember(Member member) throws Exception {
+        return memberDAO.updateMember(member);
+    }
+
+    @Override
+    public int deleteMember(Member member) throws Exception {
+
+        return memberDAO.deleteMember(member);
+    }
+
+    @Override
+    public List<Member> memberList() throws Exception {
+
+        return memberDAO.memberList();
+    }
+
+    public List<Member> searchMember(String type,String keyword) {
+
+        return memberDAO.searchMember(type,keyword);
+
+    }
+}
